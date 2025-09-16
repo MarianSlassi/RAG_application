5ecee7e3ab462e0a1f0c3a56470d3467663832fe
anon <marianslassi@MacBook-Pro.local>
2025-09-16 18:18:31 +0300

    correct gitignore

--- src/qabot/ingest/loader.py
+++ src/qabot/ingest/loader.py
@@ -7,7 +7,8 @@ class DocumentLoader():
     def load(self):
         if not self.path:
             raise ValueError("Path must be provided to load documents.")
-        # Logic to load documents from the specified path
+        
         with open(self.path, 'r') as file:
             documents = file.readlines()
+        
         return documents
